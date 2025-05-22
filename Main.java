13bf1365d8f8d8573d710163c80c9233b22d20d4
Maynor <1925597@clases.edu.sv>
2025-05-22 08:52:59 -0600

    probando las operaciones

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Operacion operacion = new Operacion();
+        // Pruebas de la clase Operacion
+        int resultado1 = operacion.sumar(5, 10);
+        System.out.println("Resultado de la suma de enteros: " + resultado1);
+        // Prueba de la suma con números negativos
+        double resultado2 = operacion.sumar(5.5, 10.5);
+        System.out.println("Resultado de la suma de dobles: " + resultado2);
+        // Prueba de la suma de tres enteros
+        int resultado3 = operacion.sumar(1, 2, 3);
+        System.out.println("Resultado de la suma de tres enteros: " + resultado3);
+        // Prueba de la suma con un número negativo
+        int resultado4 = operacion.sumar(-1, 10);
+        System.out.println("Resultado de la suma con número negativo: " + resultado4);
     }
 }
